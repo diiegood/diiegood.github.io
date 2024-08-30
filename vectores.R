7e5e79301490017624d3f2ddaaa82e600fe23928
Jorge Diego Valdez Fonseca <99843870+diiegood@users.noreply.github.com>
2024-08-29 18:44:26 -0600

    Add files via upload

---
+++ vectores.R
@@ -0,0 +1,26 @@
+##ivancruzaguilera@ciencias.unam.mx
+##vectores
+
+x<-10
+x
+#numerico
+vector<-c(26,25,23,22)
+##caracter
+bancos<-c("banamex","santander","bancomer")
+##logico
+concierto<-c(F,F,F,F)
+class(concierto)
+vector_2<-c(26,25,"banamex")
+class(vector_2) 
+#suma_vectores<-vector1+vector2#
+#extraer elementos de los vectores
+seq(1,1000,2)  #es un listado, no todos los vectores son listados".
+h<-seq(1,1000,2)
+1:100
+vector 
+vector [3]
+vector[c(2,3)]
+vector_3<-c(12,28,36,49,50)
+vector_3<-c(seq(23, 1000))
+vector_3[45]
+vector * vector_3
